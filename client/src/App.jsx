6cbc496f28c0cdbeb10f3f6935d7d6ab7f705dd6
jeyachandranj <j.jeyachandran072@gmail.com>
2024-11-07 16:01:28 +0530

    adddd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ContestForm from "./components/ContestForm";
 import ChallengeForm from "./components/ChallengeForm";
 import StudentTest from './Pages/StudentTest';
+import Project from "./Pages/Project"
 function App() {
   return (
     <Router>
@@ -9,6 +10,7 @@ function App() {
         <Route path="/" element={<ContestForm />} />
         <Route path="/challenge/:contestId" element={<ChallengeForm />} />
         <Route path="/test" element={<StudentTest />} />
+        <Route path="/project" element={<Project />} />
       </Routes>
     </Router>
   );
